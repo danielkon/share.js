3a6ffeb9cb37b1256a115c262dbc06d13654b976
DANIELKON <danielkon@live.ru>
2021-05-31 17:39:17 +0500

    Update share.js

--- share.js
+++ share.js
@@ -15,7 +15,6 @@ function share(social, options){
 	let text = options.text || "";
 	let hashtags = options.hashtags || "";
 	let result = "";
-	let target = options.target || "_self";
 
 	// Готовим ссылку для:
 	// Фейсбука
@@ -60,14 +59,7 @@ function share(social, options){
 	}
 
 	// Если условие соц. сети сработало, то херачим всплывающее окно с шарингом
-	if (result) {
-		if (target) {
-			window.open(result,target,'toolbar=0,status=0,width=626,height=436');
-		}
-		else {
-			window.location(result);
-		}
-	}
+	if (result) {window.open(result,"_blank",'toolbar=0,status=0,width=626,height=436');}
 	// Иначе посылаем пользователя в пешее эротическое путешествие
 	else {alert('Can’t share on this social network');}
 
